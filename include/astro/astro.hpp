565732d0a20bbd18c8cc1d4a91c73afeabeab6c5
datta <s.d.cherukuri@student.tudelft.nl>
2018-02-10 16:26:39 +0100

    Add rotational acceleration model without any disturbances.

--- include/astro/astro.hpp
+++ include/astro/astro.hpp
@@ -12,6 +12,7 @@
 #include "astro/j2AccelerationModel.hpp"
 #include "astro/orbitalElementConversions.hpp"
 #include "astro/twoBodyMethods.hpp"
+#include "astro/rotationalBodyAccelerationModel.hpp"
 #include "astro/solarRadiationPressureAccelerationModel.hpp"
 #include "astro/stateVectorIndices.hpp"
 
