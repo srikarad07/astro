6b962379428f977942eaa54a207416991644ad89
datta <s.d.cherukuri@student.tudelft.nl>
2018-07-20 16:07:19 +0200

    Update the cascade saturation controller, comment the part with variable limiting saturation rate.

--- include/astro/cascadeSaturationController.hpp
+++ include/astro/cascadeSaturationController.hpp
@@ -25,7 +25,7 @@ inline std::tuple< Matrix33, Matrix33, Matrix33 > computeControlGainsCascadeSatu
                                                       const Real        dampingRatio,
                                                       const Real        slewRateSaturation, 
                                                       const Vector3     principleInertiaVector, 
-                                                      const Vector4   initialQuaternion )
+                                                      const Vector4     initialQuaternion )
 {   
     //! Make a diagnal matrix out of the principle inertia vector. 
     const Matrix33 principleInertiaMatrix           = principleInertiaVector.asDiagonal(); 
@@ -38,7 +38,12 @@ inline std::tuple< Matrix33, Matrix33, Matrix33 > computeControlGainsCascadeSatu
     //! Compute the attitude gain matrices. 
     const Vector3 directionalQuaternion( initialQuaternion[0], initialQuaternion[1], initialQuaternion[2] ); 
     const Real directionalQuaternionNorm = directionalQuaternion.norm(); 
-    const Vector3 quaternionControlGainVector = angularVelocityControlGain * directionalQuaternion.array().abs() * slewRateSaturation / directionalQuaternionNorm;
+    
+    //! Calculate the quaternion gain vector as per the formula. 
+    Vector3 quaternionControlGainVector; 
+    quaternionControlGainVector[0] = ( angularVelocityControlGain * fabs(directionalQuaternion[0]) * slewRateSaturation ) / directionalQuaternionNorm;
+    quaternionControlGainVector[1] = ( angularVelocityControlGain * fabs(directionalQuaternion[1]) * slewRateSaturation ) / directionalQuaternionNorm;
+    quaternionControlGainVector[2] = ( angularVelocityControlGain * fabs(directionalQuaternion[2]) * slewRateSaturation ) / directionalQuaternionNorm;
 
     const Matrix33 attitudeControlGainMatrix = quaternionControlGainVector.asDiagonal() * principleInertiaMatrix;
 
@@ -73,6 +78,37 @@ inline Vector3 saturationFunction( const Vector3 vectorToBeSaturated,
         {
             vectorAfterSaturation[iterator] = lowerBound; 
         }
+    }
+
+    return vectorAfterSaturation; 
+}
+
+inline Vector3 minimumFunction( const Real      value1, 
+                                const Vector3   value2 )
+                    // const Real    lowerBound = -upperBound )
+{
+    // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< TO DO >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> // 
+    //! Update this function to take multiple upperbounds and be able to default the value to the only given 
+    //! upper bound if other are not mentioned (default parameter case!). Also the function should be update 
+    //! saturate for any given vector of any size. 
+    // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> //   
+
+    Vector3 vectorAfterSaturation; 
+    // const Vector3 lowerBound = - upperBound; 
+    for ( unsigned int iterator = 0; iterator < value2.size(); ++iterator )
+    {
+        if ( value1 >= value2[iterator] )
+        {
+            vectorAfterSaturation[iterator] = value2[iterator];     
+        }
+        else if ( value1 < value2[iterator] )
+        {
+            vectorAfterSaturation[iterator] = value1; 
+        }
+        // else if ( saturatedSlewRateSaturation[iterator] <= lowerBound[iterator] )
+        // {
+        //     vectorAfterSaturation[iterator] = lowerBound[iterator]; 
+        // }
     }
 
     return vectorAfterSaturation; 
@@ -135,31 +171,10 @@ inline Vector3 computeControlTorqueWithSaturationCascadeController( const Real  
                                                                     const Vector4               initialQuaternion,
                                                                     const Vector4               referenceAttitude,
                                                                     const Vector4               currentAttitude,
-                                                                    const Vector3               currentAttitudeRate, 
-                                                                    const Real                  simulationTime )
+                                                                    const Vector3               currentAttitudeRate )
 {
-    //! Compute the control gain matrices. 
-    std::tuple< Matrix33, Matrix33, Matrix33 > controlGainMatrices 
-         = computeControlGainsCascadeSaturationLogic(   naturalFrequency,
-                                                        dampingRatio,
-                                                        slewRateSaturation, 
-                                                        principleInertiaVector, 
-                                                        initialQuaternion ); 
-    // std::cout << "Is it entering?" << std::endl; 
-    const Matrix33 attitudeControlGainMatrix    = std::get<0>(controlGainMatrices); 
-    const Matrix33 attitudeControlGainMatrix2   = std::get<1>(controlGainMatrices);
-    const Matrix33 velocityControlGainMatrix    = std::get<2>(controlGainMatrices);
-    const Matrix33 principleInertiaMatrix       = principleInertiaVector.asDiagonal(); 
-    
-    const Real k = 2 * naturalFrequency * naturalFrequency; 
-    const Real c = 2 * naturalFrequency * dampingRatio; 
-    
-    // const Matrix33 attitudeControlGainMatrix    =  k * principleInertiaMatrix; 
-    // const Matrix33 attitudeControlGainMatrix2   =  ;
-    // const Matrix33 velocityControlGainMatrix    =  4 * c * principleInertiaMatrix;
-    // std::cout << "K: " << attitudeControlGainMatrix  << std::endl; 
-    // std::cout << "P: " << attitudeControlGainMatrix2 << std::endl; 
-    // std::cout << "C: " << velocityControlGainMatrix << std::endl; 
+    //! Calculate the maximum slew saturation rate. 
+    // const Vector3 maximumAcceleration( maximumControlInput[0]/principleInertiaVector[0], maximumControlInput[1]/principleInertiaVector[1], maximumControlInput[2]/principleInertiaVector[2] ); 
 
     const Vector3 initialDirectionalQuaternion( initialQuaternion[0], initialQuaternion[1], initialQuaternion[2] );
 
@@ -177,70 +192,95 @@ inline Vector3 computeControlTorqueWithSaturationCascadeController( const Real  
                                     referenceAttitude[0]*currentAttitude[1] + 
                                     referenceAttitude[3]*currentAttitude[2] - 
                                     referenceAttitude[2]*currentAttitude[3];  
-    // quaternionError[3]          =   referenceAttitude[0]*currentAttitude[0] + 
-    //                                 referenceAttitude[1]*currentAttitude[1] + 
-    //                                 referenceAttitude[2]*currentAttitude[2] + 
-    //                                 referenceAttitude[3]*currentAttitude[3];
-    // const Real endOfAccelerationPhaseTimeCalculated = 4 / angularVelocityControlGain;  
-    
-    // std::vector<Real> timeRange = linspace( initialTime, finalTime, timeStep ); 
-    // const Real endOfAccelerationPhaseTime = closest( timeRange, endOfAccelerationPhaseTimeCalculated ); 
-    
-    // std::cout << 
-    // std::cout << "The start time and end time is: " << timeRange[0] << " and " << timeRange[1] << std::endl;
-    // std::cout << "The number of times are: " << timeRange.size() << std::endl; 
-    // std::cout << "End of acceleration phase: " << endOfAccelerationPhaseTime << std::endl; 
-    // std::cout << "Simulation time: " << simulationTime << std::endl; 
 
-    // Vector4 quaternionAtEndOFAcceleration( 0.0, 0.0, 0.0, 0.0 ); 
+    // Vector3 slewRateSaturationLimiter; 
 
-    // std::cout << "Quaternion error: " << quaternionError << std::endl; 
-    // std::cout << "P * q_e " << attitudeControlGainMatrix2 * quaternionError << std::endl; 
-    // std::cout << "Saturation: " << saturationFunction( attitudeControlGainMatrix2 * quaternionError, slewRateSaturation ) << std::endl; 
-    // Real endOfCoastingPhaseTime; 
-    // const Real L = ( c / k ) * slewRateSaturation;
-    // std::cout << "L matrix: " << L << std::endl; 
-    // std::cout << "Saturation function: " << saturationFunction( attitudeControlGainMatrix * quaternionError, L ) << std::endl; 
-    // std::cout << "Control first term: " << attitudeControlGainMatrix * saturationFunction( attitudeControlGainMatrix * quaternionError, L )  << std::endl; 
-    // std::cout << "Control second term: " << velocityControlGainMatrix * currentAttitudeRate << std::endl; 
-    // std::cout << "Control first + second: " << ( attitudeControlGainMatrix * saturationFunction( quaternionError, L ) ) + ( velocityControlGainMatrix * currentAttitudeRate ) << std::endl; 
+    // slewRateSaturationLimiter[0] = sqrt( fabs( 0.4 * maximumAcceleration[0] ) ); 
+    // slewRateSaturationLimiter[1] = sqrt( fabs( 0.4 * maximumAcceleration[1] ) ); 
+    // slewRateSaturationLimiter[2] = sqrt( fabs( 0.4 * maximumAcceleration[2] ) ); 
+
+    // const Vector3 saturatedSlewRateSaturation = minimumFunction( slewRateSaturation, slewRateSaturationLimiter ); 
+
+    //! Compute the control gain matrices. 
+    std::tuple< Matrix33, Matrix33, Matrix33 > controlGainMatrices 
+         = computeControlGainsCascadeSaturationLogic(   naturalFrequency,
+                                                        dampingRatio,
+                                                        slewRateSaturation, 
+                                                        principleInertiaVector, 
+                                                        initialQuaternion ); 
+
+    const Matrix33 attitudeControlGainMatrix    = std::get<0>(controlGainMatrices); 
+    const Matrix33 attitudeControlGainMatrix2   = std::get<1>(controlGainMatrices);
+    const Matrix33 velocityControlGainMatrix    = std::get<2>(controlGainMatrices);
+    const Matrix33 principleInertiaMatrix       = principleInertiaVector.asDiagonal(); 
+    
+    const Real k = 2 * naturalFrequency * naturalFrequency; 
+    const Real c = 2 * naturalFrequency * dampingRatio; 
+    
     const Vector3 commandedControlTorque = - ( attitudeControlGainMatrix * saturationFunction( attitudeControlGainMatrix2 * quaternionError, 1.0 ) + velocityControlGainMatrix * currentAttitudeRate );  
-    // const Vector3 commandedControlTorque = - ( attitudeControlGainMatrix * saturationFunction( quaternionError, L ) + velocityControlGainMatrix * currentAttitudeRate ); 
-
-    // std::cout << "Commanded control torque: " << commandedControlTorque << std::endl; 
-    //! Compute the control torque.
-    // if ( simulationTime <= endOfAccelerationPhaseTime )
-    // {
-    //     commandedControlTorque = attitudeControlGainMatrix * saturationFunction( attitudeControlGainMatrix2 * quaternionError, slewRateSaturation ) + velocityControlGainMatrix * currentAttitudeRate;   
-    //     if ( simulationTime == endOfAccelerationPhaseTime )
-    //     {
-    //         quaternionAtEndOFAcceleration = currentAttitude; 
-    //         endOfCoastingPhaseTime = endOfAccelerationPhaseTime + 2 / slewRateSaturation * atan( initialDirectionalQuaternion.norm() / quaternionAtEndOFAcceleration[3] ); 
-    //     }
-    // }
-    // else
-    // {
-    //     if ( simulationTime <= endOfAccelerationPhaseTime )
-    //     {
-    //         commandedControlTorque = {0.0, 0.0, 0.0}; 
-    //     }
-    //     else 
-    //     {
-    //         const Real k = 2 * naturalFrequency * naturalFrequency; 
-    //         const Real c = 2 * naturalFrequency * dampingRatio; 
-    //         // commandedControlTorque = astro::computeQuaternionControlTorque( ); 
-    //         commandedControlTorque = -k*quaternionError - c*currentAttitudeRate;
-    //     }
-    //     // const Real endOfCoastingPhaseTime = endOfAccelerationPhaseTime + 2 / slewRateSaturation * atan( ); 
-    // }
     
-    // const Vector3 controlTorque = - normalizedSaturationFunction( commandedControlTorque, controlConstraint ); 
-    // std::cout << "Control torque value " << controlTorque << " at simulation time " << simulationTime << std::endl; 
-    
-    // const Vector3 controlTorque( 0.0, 0.0, 0.0);
-    // return controlTorque; 
     return commandedControlTorque; 
 }
+
+// inline Vector3 computeControlTorqueWithSaturationCascadeControllerWithLimiter( 
+//                                                                     const Real                  naturalFrequency,
+//                                                                     const Real                  dampingRatio,
+//                                                                     const Real                  slewRateSaturation, 
+//                                                                     const Vector3               principleInertiaVector, 
+//                                                                     const Vector4               initialQuaternion,
+//                                                                     const Vector4               referenceAttitude,
+//                                                                     const Vector4               currentAttitude,
+//                                                                     const Vector3               currentAttitudeRate, 
+//                                                                     const Vector3               maximumControlInput )
+// {
+//     //! Calculate the maximum slew saturation rate. 
+//     const Vector3 maximumAcceleration( maximumControlInput[0]/principleInertiaVector[0], maximumControlInput[1]/principleInertiaVector[1], maximumControlInput[2]/principleInertiaVector[2] ); 
+
+//     const Vector3 initialDirectionalQuaternion( initialQuaternion[0], initialQuaternion[1], initialQuaternion[2] );
+
+//     //! Compute the error in quaternion. 
+//     Vector3 quaternionError; 
+//     quaternionError[0]          =   referenceAttitude[3]*currentAttitude[0] + 
+//                                     referenceAttitude[2]*currentAttitude[1] - 
+//                                     referenceAttitude[1]*currentAttitude[2] - 
+//                                     referenceAttitude[0]*currentAttitude[3]; 
+//     quaternionError[1]          =  -referenceAttitude[2]*currentAttitude[0] + 
+//                                     referenceAttitude[3]*currentAttitude[1] + 
+//                                     referenceAttitude[0]*currentAttitude[2] - 
+//                                     referenceAttitude[1]*currentAttitude[3];
+//     quaternionError[2]          =   referenceAttitude[1]*currentAttitude[0] - 
+//                                     referenceAttitude[0]*currentAttitude[1] + 
+//                                     referenceAttitude[3]*currentAttitude[2] - 
+//                                     referenceAttitude[2]*currentAttitude[3];  
+
+//     Vector3 slewRateSaturationLimiter; 
+
+//     slewRateSaturationLimiter[0] = sqrt( fabs( 0.4 * maximumAcceleration[0] ) ); 
+//     slewRateSaturationLimiter[1] = sqrt( fabs( 0.4 * maximumAcceleration[1] ) ); 
+//     slewRateSaturationLimiter[2] = sqrt( fabs( 0.4 * maximumAcceleration[2] ) ); 
+
+//     const Vector3 saturatedSlewRateSaturation = minimumFunction( slewRateSaturation, slewRateSaturationLimiter ); 
+
+//     //! Compute the control gain matrices. 
+//     std::tuple< Matrix33, Matrix33, Matrix33 > controlGainMatrices 
+//          = computeControlGainsCascadeSaturationLogic(   naturalFrequency,
+//                                                         dampingRatio,
+//                                                         saturatedSlewRateSaturation, 
+//                                                         principleInertiaVector, 
+//                                                         initialQuaternion ); 
+
+//     const Matrix33 attitudeControlGainMatrix    = std::get<0>(controlGainMatrices); 
+//     const Matrix33 attitudeControlGainMatrix2   = std::get<1>(controlGainMatrices);
+//     const Matrix33 velocityControlGainMatrix    = std::get<2>(controlGainMatrices);
+//     const Matrix33 principleInertiaMatrix       = principleInertiaVector.asDiagonal(); 
+    
+//     const Real k = 2 * naturalFrequency * naturalFrequency; 
+//     const Real c = 2 * naturalFrequency * dampingRatio; 
+    
+//     const Vector3 commandedControlTorque = - ( attitudeControlGainMatrix * saturationFunction( attitudeControlGainMatrix2 * quaternionError, 1.0 ) + velocityControlGainMatrix * currentAttitudeRate );  
+    
+//     return commandedControlTorque; 
+// }
 
 } // namespace 
 
