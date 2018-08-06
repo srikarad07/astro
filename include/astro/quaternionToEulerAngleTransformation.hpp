b24c0495aaad359399561d86f450666b58f35792
datta <s.d.cherukuri@student.tudelft.nl>
2018-08-06 20:37:28 +0200

    Add the tests for quaternion to euler transformation functions.

--- include/astro/quaternionToEulerAngleTransformation.hpp
+++ include/astro/quaternionToEulerAngleTransformation.hpp
@@ -22,12 +22,22 @@ typedef Eigen::Matrix< double, 3, 1 > Vector3;
 
 inline Vector3 transformQuaternionToEulerAngles( const Vector4 quaternion )
 {
+    /*  The euler angles will be converted only in the range [-180, 180] for the roll and yaw (1st and 3rd)
+    *   angles and 80 deg for pitch (2nd) angle. This is the sequence where the euler angles acheive a 
+    *   singularity and hence the conversion sequence fails! 
+    */
+    // <<<<<<<<<<<<<<<<<<<<< TO DO >>>>>>>>>>>>>>>>>>>>>>>>>>> // 
+    // Convert to euler angles without range obligations. Use a different 
+    // conversion technique during similarities or something? 
+    // <<<<<<<<<<<<<<<<<<<<<       >>>>>>>>>>>>>>>>>>>>>>>>>>> //
+     
     Eigen::Quaterniond quaternionNew(   quaternion[3], 
                                         quaternion[0], 
                                         quaternion[1], 
                                         quaternion[2] );
-    
-    const Vector3 euler = quaternionNew.toRotationMatrix().eulerAngles(0, 1, 2);
+
+    // Use 321 rotation for the euler angle sequence.    
+    const Vector3 euler = quaternionNew.toRotationMatrix().eulerAngles(2, 1, 0);
 
     return euler;
 }
--- test/testQuaternionToEulerAngleTransformation.cpp
+++ test/testQuaternionToEulerAngleTransformation.cpp
@@ -41,16 +41,16 @@ TEST_CASE( "Test Case 1: Test the quaternion to euler angle function from Eigen.
 
 TEST_CASE( "Test Case 2: Test the quaternion to euler angle function from Eigen." , "[quaternionState]" )
 {
+    // The MATLAB function is used to compute the values of the expected euler angles. 
+    const Real tolerance = 1.0e-12; 
 
-    const Real tolerance = 1.0e-5; 
-
-    const Vector4 quaternion( 0.0905287, 0.0356992, 0.0905287, 0.991128 ); 
+    const Vector4 quaternion( 1.767766952966369e-01, 3.061862178478972e-01, 1.767766952966369e-01, 9.185586535436919e-01 ); 
 
     // computedEulerAngles; 
     const Vector3 computedEulerAngles = transformQuaternionToEulerAngles( quaternion );  
      
-    // Euler angles 10 deg, 5 deg, 10 deg in radians.  
-    const Vector3 expectedEulerAngles(  1.745329251994329e-01,  8.726646259971647e-02,  1.745329251994329e-01 ); 
+    // Euler angles 30 deg, 30 deg, 30 deg in radians.  
+    const Vector3 expectedEulerAngles( 5.235987755982987e-01, 5.235987755982989e-01, 5.235987755982988e-01 ); 
 
     // Check if expected values are equal to the computed values. 
     REQUIRE( computedEulerAngles[0] == Approx( expectedEulerAngles[0]).epsilon(tolerance) ); 
@@ -60,6 +60,95 @@ TEST_CASE( "Test Case 2: Test the quaternion to euler angle function from Eigen.
     REQUIRE( computedEulerAngles[2] == Approx( expectedEulerAngles[2]).epsilon(tolerance) );  
 
 } // test case 2
+
+
+TEST_CASE( "Test Case 3: Test the quaternion to euler angle function from Eigen." , "[quaternionState]" )
+{
+    // The MATLAB function is used to compute the values of the expected euler angles. 
+    const Real tolerance = 1.0e-12; 
+
+    const Vector4 quaternion( 1.920882315810498e-01, 4.119345419740040e-01, 1.920882315810498e-01, 8.697782221375732e-01 ); 
+
+    // computedEulerAngles; 
+    const Vector3 computedEulerAngles = transformQuaternionToEulerAngles( quaternion );  
+     
+    // Euler angles 40 deg, 40 deg, 40 deg in radians.  
+    const Vector3 expectedEulerAngles( 6.981317007977320e-01, 6.981317007977319e-01, 6.981317007977320e-01 ); 
+
+    // Check if expected values are equal to the computed values. 
+    REQUIRE( computedEulerAngles[0] == Approx( expectedEulerAngles[0]).epsilon(tolerance) ); 
+
+    REQUIRE( computedEulerAngles[1] == Approx( expectedEulerAngles[1]).epsilon(tolerance) ); 
+    
+    REQUIRE( computedEulerAngles[2] == Approx( expectedEulerAngles[2]).epsilon(tolerance) );  
+
+} // test case 3
+
+TEST_CASE( "Test Case 4: Test the quaternion to euler angle function from Eigen." , "[quaternionState]" )
+{
+    // The MATLAB function is used to compute the values of the expected euler angles. 
+    const Real tolerance = 1.0e-12; 
+
+    const Vector4 quaternion( 4.657518160827175e-01, 3.253559543278105e-01, -9.860578031679113e-03, 8.228739379759037e-01 ); 
+
+    // computedEulerAngles; 
+    const Vector3 computedEulerAngles = transformQuaternionToEulerAngles( quaternion );  
+     
+    // Euler angles 20.0, 33.0, 65.0 in radians.  
+    const Vector3 expectedEulerAngles( 3.490658503988659e-01, 5.759586531581288e-01, 1.134464013796314e+00 ); 
+
+    // Check if expected values are equal to the computed values. 
+    REQUIRE( computedEulerAngles[0] == Approx( expectedEulerAngles[0]).epsilon(tolerance) ); 
+
+    REQUIRE( computedEulerAngles[1] == Approx( expectedEulerAngles[1]).epsilon(tolerance) ); 
+    
+    REQUIRE( computedEulerAngles[2] == Approx( expectedEulerAngles[2]).epsilon(tolerance) );  
+
+} // test case 4
+
+TEST_CASE( "Test Case 5: Test the quaternion to euler angle function from Eigen." , "[quaternionState]" )
+{
+    // The MATLAB function is used to compute the values of the expected euler angles. 
+    const Real tolerance = 1.0e-12; 
+
+    const Vector4 quaternion( 2.280970497069847e-01, 5.716378203564888e-01, -1.165743576888184e-02, 7.880774342945918e-01 ); 
+
+    // computedEulerAngles; 
+    const Vector3 computedEulerAngles = transformQuaternionToEulerAngles( quaternion );  
+     
+    // Euler angles 20.0, 33.0, 65.0 in radians.  
+    const Vector3 expectedEulerAngles( 6.108652381980152e-01, 1.134464013796315e+00, 9.599310885968811e-01 ); 
+
+    // Check if expected values are equal to the computed values. 
+    REQUIRE( computedEulerAngles[0] == Approx( expectedEulerAngles[0]).epsilon(tolerance) ); 
+
+    REQUIRE( computedEulerAngles[1] == Approx( expectedEulerAngles[1]).epsilon(tolerance) ); 
+    
+    REQUIRE( computedEulerAngles[2] == Approx( expectedEulerAngles[2]).epsilon(tolerance) );  
+
+} // test case 5
+
+TEST_CASE( "Test Case 6: Test the quaternion to euler angle function from Eigen." , "[quaternionState]" )
+{
+    // The MATLAB function is used to compute the values of the expected euler angles. 
+    const Real tolerance = 1.0e-12; 
+
+    const Vector4 quaternion( 2.258941546823033e-01, 7.239915037281760e-01, -1.387384119346450e-01, 6.368357609805178e-01 ); 
+
+    // computedEulerAngles; 
+    const Vector3 computedEulerAngles = transformQuaternionToEulerAngles( quaternion );  
+     
+    // Euler angles 120, 80, 150 in radians.  
+    const Vector3 expectedEulerAngles( 2.094395102393195e+00, 1.396263401595465e+00, 2.617993877991494e+00); 
+
+    // Check if expected values are equal to the computed values. 
+    REQUIRE( computedEulerAngles[0] == Approx( expectedEulerAngles[0]).epsilon(tolerance) ); 
+
+    REQUIRE( computedEulerAngles[1] == Approx( expectedEulerAngles[1]).epsilon(tolerance) ); 
+    
+    REQUIRE( computedEulerAngles[2] == Approx( expectedEulerAngles[2]).epsilon(tolerance) );  
+
+} // test case 6
 
 } // tests
 
